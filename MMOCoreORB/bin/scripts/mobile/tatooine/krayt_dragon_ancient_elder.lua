413ad7109a85db558cbca04f1176fdff841f1ff7
merik444 <whitelight_illumina@hotmail.com>
2016-09-06 03:39:26 -0400

    Revert "[Added] New Versions to current Spawns"
    This reverts commit 1fcae1b2aeab590d383111a998f8ad0becec552a.

--- MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_ancient_elder.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/krayt_dragon_ancient_elder.lua
@@ -27,42 +27,24 @@ krayt_dragon_ancient_elder = Creature:new {
  	diet = CARNIVORE,
  	scale = 1.5,
  
-	templates = {"object/mobile/krayt_dragon.iff"},
-	lootGroups = {
-		{
-			groups = {
-					{group = "krayt_tissue_rare", chance = 3000000},
-					{group = "krayt_dragon_common2", chance = 3000000},
-					{group = "pearls_flawless", chance = 3000000},
-					{group = "clothing_attachments", chance = 1000000},
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "saberhand24", chance = 10000000}
-			},
-			lootChance = 1000000
-		},
-		{
-			groups = {
-				{group = "saberhand25", chance = 10000000}
-			},
-			lootChance = 1000000
-		},
-		{
-			groups = {
-				{group = "saberhand26", chance = 10000000}
-			},
-			lootChance = 1000000
-		},
-	},
-	weapons = {},
-	conversationTemplate = "",
-	attacks = {
-		{"creatureareacombo","stateAccuracyBonus=100"},
-		{"creatureareaknockdown","stateAccuracyBonus=100"}
-	}
-}
+ 	templates = {"object/mobile/krayt_dragon.iff"},
+ 	lootGroups = {
+ 		{
+ 	        groups = {
+ 					{group = "krayt_tissue_rare", chance = 3000000},
+ 					{group = "krayt_dragon_common2", chance = 2000000},
+ 					{group = "pearls_flawless", chance = 3000000},
+ 					{group = "clothing_attachments", chance = 2000000},
+ 			},
+ 			lootChance = 10000000
+ 		}
+ 	},
+ 	weapons = {},
+ 	conversationTemplate = "",
+ 	attacks = {
+ 		{"creatureareacombo","stateAccuracyBonus=100"},
+ 		{"creatureareaknockdown","stateAccuracyBonus=100"}
+ 	}
+ }
  
  CreatureTemplates:addCreatureTemplate(krayt_dragon_ancient_elder, "krayt_dragon_ancient_elder")
