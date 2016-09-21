d69c3fbf166fc5e3bb76fad1ef1c3c0b524e3188
merik444 <whitelight_illumina@hotmail.com>
2016-09-21 15:32:12 -0400

    Revert "[Added] Combat Rebalance P.2"
    This reverts commit 2dd9588e79158d640e3d42d89ab6f38d8ada067e.

--- MMOCoreORB/bin/scripts/commands/unarmedLegHit1.lua
+++ MMOCoreORB/bin/scripts/commands/unarmedLegHit1.lua
@@ -44,7 +44,7 @@
 UnarmedLegHit1Command = {
         name = "unarmedleghit1",
 
-	damageMultiplier = 3.75,
+	damageMultiplier = 2.5,
 	speedMultiplier = 2.0,
 	healthCostMultiplier = 1.75,
 	actionCostMultiplier = 1.75,
--- MMOCoreORB/bin/scripts/commands/unarmedLunge2.lua
+++ MMOCoreORB/bin/scripts/commands/unarmedLunge2.lua
@@ -44,7 +44,7 @@
 UnarmedLunge2Command = {
 	name = "unarmedlunge2",
 
-	damageMultiplier = 4.50,
+	damageMultiplier = 3.0,
 	speedMultiplier = 2.0,
 	accuracyBonus = 15,
 	
