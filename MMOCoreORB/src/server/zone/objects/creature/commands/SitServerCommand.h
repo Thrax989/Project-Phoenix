e6e646da36bf4e14044de0a796f2f66520af169c
Thrax989 <thrax989@yahoo.com>
2016-09-21 06:32:57 -0700

    [Added] 2 New Functions to /Sit And /Prone Commands Click + To See Description
    These Commands Now Allow Neutral Players To Participate In The Galacit
    Civil War
    /Sit Now Puts Any Player Rebel  Imperial Or Neutral On Leave At Any
    Time.
    /Prone Command Puts Any Player Rebel Imperial Neutral On Leave Making
    Them Red To All 3 Factions Rebels Imperials And Neutral’s Can Attack
    Each other Now At Any Time When Activating The Command.
    Added Global Broadcasting In To Enforce People Not To Abuse The Commands
    Broadcasting Will List Players Name If They Use These Commands To Try To
    Cheat Pvp They Will Be Dealt With
    When In Combat You Cannot Sit To Escape Going On Leave This Will Enforce
    People Not To Cheat There Way Out Of A Easy Or Hard Fight.
    More Checks Will Be Added Soon This Is The First Step To Making Neutral
    Imperial And Rebel Overt and Onleave Functions In The Game.
    Remember These Commands Can Be Used Anywhere You Don’t Need To See A
    Recruiter Now To Go On Leave Or Overt.

--- MMOCoreORB/src/server/zone/objects/creature/commands/SitServerCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/SitServerCommand.h
@@ -1,6 +1,13 @@
 /*
 				Copyright <SWGEmu>
 		See file COPYING for copying conditions.*/
+/*
+ * PLEASE DO NOT STEAL OUR WORK
+ * ASK BEFOR USING
+ * Contact Me Here http://projectphoenix.com.shivtr.com/
+ * Created on: 9/21/2016
+ * Authors: TOXIC
+ */
 
 #ifndef SITSERVERCOMMAND_H_
 #define SITSERVERCOMMAND_H_
@@ -9,6 +16,9 @@
 
 #include "server/zone/packets/object/SitOnObject.h"
 #include "templates/params/creature/CreatureState.h"
+#include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
 
 class SitServerCommand : public QueueCommand {
 public:
@@ -31,6 +41,14 @@ public:
 
 		if (arguments.isEmpty()) {
 			creature->setPosture(CreaturePosture::SITTING);
+			PlayerObject* targetGhost = creature->getPlayerObject();
+			targetGhost->setFactionStatus(FactionStatus::ONLEAVE);
+			Zone* zone = creature->getZone();
+			//Broadcast to Server
+ 			String playerName = creature->getFirstName();
+ 			StringBuffer zBroadcast;
+ 			zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Is Now OnLeave";
+ 			creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
 		} else {
 			StringTokenizer tokenizer(arguments.toString());
 			tokenizer.setDelimeter(",");
@@ -80,4 +98,3 @@ public:
 };
 
 #endif //SITSERVERCOMMAND_H_
-
