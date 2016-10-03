3ddb3172cf9e98a862f04fe67a6bfa570bd91c45
TOXIC <thrax989@yahoo.com>
2016-10-03 10:13:41 -0700

    Change
    Change

--- MMOCoreORB/bin/scripts/commands/forceIntimidate2.lua
+++ MMOCoreORB/bin/scripts/commands/forceIntimidate2.lua
@@ -46,7 +46,7 @@ ForceIntimidate2Command = {
 
 	damageMultiplier = 0,
 	speed = 0.0,
-	forceCost = 200,
+	forceCost = 250,
 	accuracySkillMod = "forceintimidate_accuracy",
 	forceAttack = true,
 	splashDamage = true,
