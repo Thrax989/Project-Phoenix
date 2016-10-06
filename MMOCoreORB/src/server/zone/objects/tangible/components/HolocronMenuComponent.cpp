803dba3b72503599119632145e7c4e61e344a145
TOXIC <thrax989@yahoo.com>
2016-10-06 16:07:05 +0000

    [Added] Visibility Check On Holocrons Moved It Off Of Force Crystals
    [Added] Visibility Check On Holocrons Moved It Off Of Force Crystals

--- MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/HolocronMenuComponent.cpp
@@ -12,6 +12,7 @@
 #include "server/zone/ZoneServer.h"
 #include "server/zone/managers/jedi/JediManager.h"
 #include "server/chat/ChatManager.h"
+#include "server/zone/packets/player/PlayMusicMessage.h"
 #include "server/zone/managers/visibility/VisibilityManager.h"
 #include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
 #include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
@@ -24,14 +25,25 @@ int HolocronMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, Crea
 	if (!sceneObject->isASubChildOf(creature))
 		return 0;
 
+	ManagedReference<PlayerObject*> ghost = creature->getPlayerObject();
+	if (ghost == NULL)
+	return 0;
+
 	JediManager::instance()->useItem(sceneObject, JediManager::ITEMHOLOCRON, creature);
         creature->playEffect("clienteffect/pl_force_absorb_hit.cef");
         sceneObject->destroyObjectFromWorld(true);
-	//Broadcast to Server
-	String playerName = creature->getFirstName();
-	StringBuffer zBroadcast;
-	zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Has Used A Holocron";
-	creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
+			int jediVis1 = ghost->getVisibility();
+			PlayMusicMessage* pmm = new PlayMusicMessage("sound/music_become_light_jedi.snd");
+  			ghost->sendMessage(pmm);
+			Zone* zone = creature->getZone();
+			//Broadcast to Server
+ 			String playerName = creature->getFirstName();
+ 			StringBuffer zBroadcast;
+ 			zBroadcast << "\\#00E604" << playerName << " \\#63C8F9 Has Used A Holocron";
+ 			creature->getZoneServer()->getChatManager()->broadcastGalaxy(NULL, zBroadcast.toString());
+			StringBuffer messageVis;
+			messageVis << "\\#00CC00 Your Visibility is at: " << jediVis1;
+			creature->sendSystemMessage(messageVis.toString());
 
 	return 0;
 }
