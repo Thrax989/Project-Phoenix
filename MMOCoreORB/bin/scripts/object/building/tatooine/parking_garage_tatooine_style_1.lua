ee3ec3afe8ac0ce2a561b2e4e0633101a818972d
Thrax989 <thrax989@yahoo.com>
2016-10-10 14:31:04 -0700

    [Added] Missing Parking Garage Date Component
    [Added] Missing Parking Garage Date Component

--- MMOCoreORB/bin/scripts/object/building/tatooine/parking_garage_tatooine_style_1.lua
+++ MMOCoreORB/bin/scripts/object/building/tatooine/parking_garage_tatooine_style_1.lua
@@ -43,7 +43,9 @@
 
 object_building_tatooine_parking_garage_tatooine_style_1 = object_building_tatooine_shared_parking_garage_tatooine_style_1:new {
 	gameObjectType = 528,
-	planetMapCategory = "garage"
+	planetMapCategory = "garage",
+	zoneComponent = "GarageZoneComponent",
+	dataObjectComponent = "GarageDataComponent",
 }
 
 ObjectTemplates:addTemplate(object_building_tatooine_parking_garage_tatooine_style_1, "object/building/tatooine/parking_garage_tatooine_style_1.iff")
