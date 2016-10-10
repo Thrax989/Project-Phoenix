30d5d678e62aa57910c6f7aff9e6c288e031ca24
Thrax989 <thrax989@yahoo.com>
2016-10-10 08:49:07 -0700

    Revert "[Event] BlueLeaf Invasion"
    This reverts commit d5bc1319c0edbeba79cf6b4b8d3ff90bf929331e.

--- MMOCoreORB/bin/scripts/screenplays/poi/yavin4_blueleaf_temple.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/yavin4_blueleaf_temple.lua
@@ -42,11 +42,28 @@ end
 
 function BlueleafTempleScreenPlay:spawnMobiles()
 	-- Outside Temple	
-	spawnMobile("yavin4", "elliot",180,-895.6,84.8,-2059,-139,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-895.6,84.8,-2059,-139,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-896.2,84.8,-2064.1,42,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-892.3,84.8,-2062.4,-109,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-884.4,84.8,-2026.1,-96,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-888.5,84.8,-2023.3,155,0)
+	spawnMobile("yavin4", "kliknik_dark_hunter",600,-888.6,84.8,-2028.8,35,0)
 	
 	-- Inside Temple
-	spawnMobile("yavin4", "Mihpisto",180,-9,-37.3,-9,48,468319)
-	spawnMobile("yavin4", "marvolo",180,-0.6,-37.3,-28.3,-19,468319)
-	spawnMobile("yavin4", "mittens",180,-24,-1.3,10.3,93,468319)
-	spawnMobile("yavin4", "Zamidirin",180,12.4,-1.3,32.4,-93,468318)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,-9,-37.3,-9,48,468319)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,-10.5,-37.3,4.2,111,468319)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,6.6,-37.3,12.8,-149,468319)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,10.2,-37.3,4.8,-110,468319)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,10.9,-37.3,-3.9,-69,468319)
+	spawnMobile("yavin4", "kliknik_dark_defender",600,-12.1,-37.3,18.2,74,468319)
+	
+	spawnMobile("yavin4", "kliknik_dark_queen",600,-0.6,-37.3,-28.3,-19,468319)
+	
+	spawnMobile("yavin4", "kliknik_dark_worker",600,-24,-1.3,10.3,93,468319)
+	spawnMobile("yavin4", "kliknik_dark_worker",600,-16.4,-1.3,-21,49,468319)
+	spawnMobile("yavin4", "kliknik_dark_worker",600,12.1,-1.3,-23.4,21,468319)
+	spawnMobile("yavin4", "kliknik_dark_worker",600,23.4,-1.3,11,-77,468319)
+	
+	spawnMobile("yavin4", "kliknik_dark_warrior",600,12.4,-1.3,32.4,-93,468318)
+	spawnMobile("yavin4", "kliknik_dark_warrior",600,-12.8,-1.3,32,93,468318)
 end
