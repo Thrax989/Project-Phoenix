1d5de669b197f28059496965ec1dd2959bd7c6df
bobvious <bob@shew.com>
2016-10-20 03:48:27 -0700

    totalHealOther to 1500 and force cost dependent on heal amount
    Max force cost is still 800, but actual cost is now dependent on the amount of damage and wounds healed.    States and DOT's are not additional force cost, which is a master healer benefit.    Increased damage heal amount from 1000 to 1500 and increased battle fatigue and wound healing nominally.

--- MMOCoreORB/bin/scripts/commands/totalHealOther.lua
+++ MMOCoreORB/bin/scripts/commands/totalHealOther.lua
@@ -48,12 +48,12 @@ TotalHealOtherCommand = {
 	healPoison = 450,
 	healBleeding = 450,
 	forceCost = 800,
-	healAmount = 1000,
+	healAmount = 1500,
 	healAttributes = HEALTH_ATTRIBUTE + MIND_ATTRIBUTE + ACTION_ATTRIBUTE,
 	healWoundAttributes = HEALTH_ATTRIBUTE + MIND_ATTRIBUTE + ACTION_ATTRIBUTE,
-	healBattleFatigue = 250,
-	healWoundAmount = 250,
-	forceCostDivisor = 0,
+	healBattleFatigue = 500,
+	healWoundAmount = 500,
+	forceCostDivisor = 9.5,
 	range = 32	
 }
 
