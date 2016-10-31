6a3fe394a6a15e696e1326861593303b32a1efaf
merik444 <whitelight_illumina@hotmail.com>
2016-10-31 10:42:34 -0400

    [Fixed] Halloween Event to use the proper elder
    ~ Replaced Elder with the Elder_2's. Reset the original Elder back to
    older version. Faction pet's should now show with the correct resist
    again

--- MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_elder.lua
@@ -5,14 +5,14 @@ randomNameTag = true,
 socialGroup = "nightsister",
 faction = "",
 level = 300,
-chanceHit = 65.00,
-damageMin = 1500,
-damageMax = 2500,
-baseXp = 27849000,
-baseHAM = 2500000,
-baseHAMmax = 3000000,
+chanceHit = 68.75,
+damageMin = 3850,
+damageMax = 3950,
+baseXp = 26654,
+baseHAM = 400000,
+baseHAMmax = 410000,
 armor = 3,
-resists = {35,35,35,35,35,35,35,35,35},
+resists = {200,200,200,200,200,200,200,200,50},
 meatType = "",
 meatAmount = 0,
 hideType = "",
@@ -29,252 +29,24 @@ diet = HERBIVORE,
 
         templates = {"object/mobile/dressed_dathomir_nightsister_elder.iff"},
          lootGroups = {
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "g_pistol_heroic_exar", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "halloweenevent1", chance = 5000000},
-				{group = "halloweenevent2", chance = 2500000},
-				{group = "halloweenevent3", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "halloweenevent4", chance = 5000000},
-				{group = "halloweenevent5", chance = 2500000},
-				{group = "halloweenevent6", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "halloweenevent7", chance = 5000000},
-				{group = "halloweenevent8", chance = 2500000},
-				{group = "halloweenevent9", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "halloweenevent10", chance = 5000000},
-				{group = "halloweenevent11", chance = 2500000},
-				{group = "halloweenevent12", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "halloweenevent13", chance = 5000000},
-				{group = "halloweenevent14", chance = 2500000},
-				{group = "halloweenevent15", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		  {
-			groups = {
-				{group = "nyms_rare", chance = 3000000},
-				{group = "g_pistol_heroic_exar", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "saberhand20", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "saberhand18", chance = 3000000},
-				{group = "saberhand19", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "weapons_all", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "g_pistol_heroic_exar", chance = 3000000},
-				{group = "aakuan_common", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "weapons_all", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "g_pistol_heroic_sd", chance = 3000000},
-				{group = "g_pistol_heroic_exar", chance = 2000000},
-				{group = "g_sword_pvp_bf_01", chance = 1000000},
-				{group = "g_pvp_bf_knuckler", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "g_carbine_heroic_sd", chance = 3000000},
-				{group = "g_carbine_kun_massassi", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "g_polearm_heroic_sd", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "g_carbine_heroic_sd", chance = 500000},
-				{group = "g_carbine_kun_massassi", chance = 500000},
-				{group = "g_polearm_heroic_sd", chance = 500000},
-				{group = "g_pvp_bf_knuckler", chance = 500000},
-				{group = "g_sword_pvp_bf_01", chance = 500000},
-				{group = "g_pistol_heroic_exar", chance = 500000},
-				{group = "g_pistol_heroic_sd", chance = 500000},
-				{group = "g_som_2h_sword_obsidian", chance = 500000},
-				{group = "g_som_2h_sword_tulrus", chance = 500000},
-				{group = "g_som_sword_obsidian", chance = 500000},
-				{group = "saberhand17", chance = 4300000},
-				{group = "g_ep3_chiss_poacher_backpack", chance = 200000},
-				{group = "g_ep3_loot_necrosis", chance = 500000}
-			},
-			lootChance = 10000000
-		},
-		  {
-			groups = {
-				{group = "g_ep3_chiss_poacher_backpack", chance = 10000000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "g_ep3_loot_necrosis", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "g_som_2h_sword_tulrus", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "g_som_sword_obsidian", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "clothing_attachments", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		{
-			groups = {
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "armor_attachments", chance = 2500000},
-				{group = "g_som_sword_obsidian", chance = 2500000}
-			},
-			lootChance = 10000000
-		},
-		  {
-			groups = {
-				{group = "saberhand15", chance = 3000000},
-				{group = "saberhand16", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "g_ep3_loot_necrosis", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "saberhand14", chance = 3000000},
-				{group = "g_som_2h_sword_tulrus", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "weapons_all", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "saberhand13", chance = 3000000},
-				{group = "saberhand12", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "weapons_all", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "nyms_rare", chance = 3000000},
-				{group = "aakuan_common", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "weapons_all", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "nyms_rare", chance = 3000000},
-				{group = "aakuan_common", chance = 2000000},
-				{group = "g_named_crystals", chance = 1000000},
-				{group = "g_ep3_loot_necrosis", chance = 4000000}
-		},
-			lootChance = 10000000
-		  },
-		  {
-			groups = {
-				{group = "g_carbine_heroic_sd", chance = 500000},
-				{group = "g_carbine_kun_massassi", chance = 500000},
-				{group = "g_polearm_heroic_sd", chance = 500000},
-				{group = "g_pvp_bf_knuckler", chance = 500000},
-				{group = "g_sword_pvp_bf_01", chance = 500000},
-				{group = "g_pistol_heroic_exar", chance = 500000},
-				{group = "g_pistol_heroic_sd", chance = 500000},
-				{group = "g_som_2h_sword_obsidian", chance = 500000},
-				{group = "g_som_2h_sword_tulrus", chance = 500000},
-				{group = "g_som_sword_obsidian", chance = 500000},
-				{group = "weapons_all", chance = 4300000},
-				{group = "g_ep3_chiss_poacher_backpack", chance = 200000},
-				{group = "g_ep3_loot_necrosis", chance = 500000}
-			},
-			lootChance = 10000000
-		},
-		  {
-			groups = {
-				{group = "saberhand17", chance = 10000000}
-			},
-			lootChance = 10000000
-		},
-		},
+                 {
+                         groups = {
+                                 {group = "crystals_premium", chance = 800000},
+                                {group = "nightsister_common", chance = 2200000},
+                                  {group = "junk", chance = 1000000},
+                                 {group = "armor_attachments", chance = 500000},
+                                  {group = "clothing_attachments", chance = 500000},
+                                  {group = "pistols", chance = 800000},
+                                  {group = "rifles", chance = 800000},
+                                  {group = "carbines", chance = 900000},
+                                  {group = "melee_weapons", chance = 2000000},
+                                  {group = "wearables_scarce", chance = 500000}
+                          }
+                  }
+          },
           weapons = {"mixed_force_weapons"},
-         reactionStf = "@npc_reaction/slang",
-         attacks = merge(tkamaster,swordsmanmaster,fencermaster,pikemanmaster,brawlermaster)
+         conversationTemplate = "",
+         attacks = merge(tkamaster,swordsmanmaster,fencermaster,pikemanmaster,brawlermaster,forcepowermaster)
  }
  
   CreatureTemplates:addCreatureTemplate(nightsister_elder, "nightsister_elder")
