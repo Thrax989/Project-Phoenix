cfd06cdbdf556275b2bf8758a27f351cf04e0b04
Thrax989 <thrax989@yahoo.com>
2016-10-31 21:15:07 -0700

    [Added] Crafting Objects
    [Added] Crafting Objects

--- MMOCoreORB/bin/scripts/object/tangible/crafting/station/jedi_tool_crit_assembly_5.lua
+++ MMOCoreORB/bin/scripts/object/tangible/crafting/station/jedi_tool_crit_assembly_5.lua
@@ -41,33 +41,49 @@
 --this exception also makes it possible to release a modified version 
 
 
+object_tangible_crafting_station_jedi_tool_crit_assembly_5 = object_tangible_crafting_station_shared_jedi_tool:new {
 
---Children folder includes
+	templateType = CRAFTINGTOOL,
 
--- Server Objects
-includeFile("tangible/crafting/station/armor_repair.lua")
-includeFile("tangible/crafting/station/clothing_repair.lua")
-includeFile("tangible/crafting/station/clothing_station.lua")
-includeFile("tangible/crafting/station/clothing_tool.lua")
-includeFile("tangible/crafting/station/droid_repair.lua")
-includeFile("tangible/crafting/station/food_station.lua")
-includeFile("tangible/crafting/station/food_tool.lua")
-includeFile("tangible/crafting/station/generic_tool.lua")
-includeFile("tangible/crafting/station/jedi_tool.lua")
-includeFile("tangible/crafting/station/jedi_tool_crit_assembly_5.lua")
-includeFile("tangible/crafting/station/jedi_tool_crit_assembly_20.lua")
-includeFile("tangible/crafting/station/jedi_tool_crit_experiment_5.lua")
-includeFile("tangible/crafting/station/jedi_tool_crit_experiment_20.lua")
-includeFile("tangible/crafting/station/public_clothing_station.lua")
-includeFile("tangible/crafting/station/public_food_station.lua")
-includeFile("tangible/crafting/station/public_space_station.lua")
-includeFile("tangible/crafting/station/public_structure_station.lua")
-includeFile("tangible/crafting/station/public_weapon_station.lua")
-includeFile("tangible/crafting/station/space_station.lua")
-includeFile("tangible/crafting/station/space_tool.lua")
-includeFile("tangible/crafting/station/structure_repair.lua")
-includeFile("tangible/crafting/station/structure_station.lua")
-includeFile("tangible/crafting/station/structure_tool.lua")
-includeFile("tangible/crafting/station/weapon_repair.lua")
-includeFile("tangible/crafting/station/weapon_station.lua")
-includeFile("tangible/crafting/station/weapon_tool.lua")
+	playerRaces = { "object/creature/player/bothan_male.iff",
+				"object/creature/player/bothan_female.iff",
+				"object/creature/player/human_male.iff",
+				"object/creature/player/human_female.iff",
+				"object/creature/player/ithorian_male.iff",
+				"object/creature/player/ithorian_female.iff",
+				"object/creature/player/moncal_male.iff",
+				"object/creature/player/moncal_female.iff",
+				"object/creature/player/rodian_male.iff",
+				"object/creature/player/rodian_female.iff",
+				"object/creature/player/sullustan_male.iff",
+				"object/creature/player/sullustan_female.iff",
+				"object/creature/player/trandoshan_male.iff",
+				"object/creature/player/trandoshan_female.iff",
+				"object/creature/player/twilek_male.iff",
+				"object/creature/player/twilek_female.iff",
+				"object/creature/player/wookiee_male.iff",
+				"object/creature/player/wookiee_female.iff",
+				"object/creature/player/zabrak_male.iff",
+				"object/creature/player/zabrak_female.iff" },
+
+	customizationOptions = {},
+	customizationDefaults = {},
+	
+	forceCriticalAssembly = 5, -- Guaranteed critical assembly
+
+	toolType = 4,
+	complexityLevel = 20,
+	enabledTabs = {2048},
+
+	numberExperimentalProperties = {1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX", "CD"},
+	experimentalWeights = {1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null", "exp_effectiveness"},
+	experimentalSubGroupTitles = {"null", "null", "hitpoints", "usemodifier"},
+	experimentalMin = {0, 0, 1000, -15},
+	experimentalMax = {0, 0, 1000, 15},
+	experimentalCombineType = {0, 0, 4, 1},
+	experimentalPrecision = {0, 0, 0, 0},
+	experimentalCombineType = {1, 1, 1, 1},
+}
+ObjectTemplates:addTemplate(object_tangible_crafting_station_jedi_tool_crit_assembly_5, "object/tangible/crafting/station/jedi_tool_crit_assembly_5.iff")
