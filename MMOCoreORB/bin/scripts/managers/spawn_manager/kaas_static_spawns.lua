459d0dd0160d13df7dd42aba26d3791e4f53aa0f
Thrax989 <thrax989@yahoo.com>
2016-10-31 18:40:33 -0700

    [Added[ Updated World Region Spawns
    [Added[ Updated World Region Spawns

---
+++ MMOCoreORB/bin/scripts/managers/spawn_manager/kaas_static_spawns.lua
@@ -0,0 +1,8 @@
+require("scripts.managers.spawn_manager.static_spawns")
+
+kaas_static_spawns = {
+	{"imperial_recruiter",60,-5106,80,-2286,0,0, "neutral", "", ""},
+	{"rebel_recruiter",60,2835,126,3881,90,0, "neutral", "", ""},
+
+
+}
