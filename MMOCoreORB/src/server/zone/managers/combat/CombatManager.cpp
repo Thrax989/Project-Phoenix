0a7a1b56d6ba989121f2489e22b2472734a651fc
Thrax989 <thrax989@yahoo.com>
2016-11-10 17:33:36 -0800

    [Added] Min and Max Weapon Damage Checks Set To Change Once Server Votes A Max Min And A Max Maximum damge for the server.

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -290,12 +290,12 @@ int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* 
 
 int CombatManager::doTargetCombatAction(CreatureObject* attacker, WeaponObject* weapon, CreatureObject* defender, const CreatureAttackData& data) {
 	if (weapon->getMinDamage() < 1 ||
-			weapon->getMinDamage() > 50000 ||
+			weapon->getMinDamage() > 3000 ||
 			weapon->getMaxDamage() < 1 ||
-			weapon->getMaxDamage() > 50000) {
+			weapon->getMaxDamage() > 3500) {
 		Locker locker(weapon);
-		weapon->setMinDamage(5);
-		weapon->setMaxDamage(10);
+		weapon->setMinDamage(1500);
+		weapon->setMaxDamage(2000);
 		info(attacker->getFirstName() + " was found using a bugged weapon!!", true);
 	}
 	if (defender->isEntertaining())
