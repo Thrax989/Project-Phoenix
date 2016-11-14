d6003ca3c4c0a942214a7bc7512be687b9507cb3
merik444 <whitelight_illumina@hotmail.com>
2016-11-14 04:26:56 -0500

    [Edited] Enrage pets Command
    ~Changed the damage bonus on enraging pets from 50% to 175% to
    compensate for the massive damage loss the weapon limiter has caused.
    
    ~ Server will need a rebuild before it takes effect

--- MMOCoreORB/src/server/zone/objects/creature/commands/EnragepetsCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/EnragepetsCommand.h
@@ -22,7 +22,7 @@ public:
 	int doQueueCommand(CreatureObject* player, const uint64& target, const UnicodeString& arguments) const {
 
 		int cooldownMilli = 240000; // 4 min
-		int durationSec =  210; // 3.5 min
+		int durationSec =  230; // 3.5 min
 		int mindCost = player->calculateCostAdjustment(CreatureAttribute::FOCUS, 100 );
 		unsigned int buffCRC = STRING_HASHCODE("enragePet");
 
@@ -78,8 +78,8 @@ public:
 				if( pet->getCooldownTimerMap() == NULL || !pet->getCooldownTimerMap()->isPast("enragePetsCooldown") )
 					continue;
 
-				// Determine damage bonus (50% of average damage)
-				int damageBonus = (int) ((((float)pet->getDamageMin() + (float)pet->getDamageMax())/2) * 0.50);
+				// Determine damage bonus (175% of average damage)
+				int damageBonus = (int) ((((float)pet->getDamageMin() + (float)pet->getDamageMax())/2) * 1.75);
 
 				// Determine damage susceptibility (one quarter of damage bonus)
 				int damageSusceptibility = damageBonus / 4;
