cbf844a816ef79cb9474fc7bb8d39a6055d7eb98
Thrax989 <thrax989@yahoo.com>
2016-11-15 23:23:24 -0800

    [Added] Test Creature Deed

--- MMOCoreORB/bin/scripts/object/tangible/deed/ep3_pet_deed/varactyl_deed.lua
+++ MMOCoreORB/bin/scripts/object/tangible/deed/ep3_pet_deed/varactyl_deed.lua
@@ -41,22 +41,8 @@
 --this exception also makes it possible to release a modified version 
 
 
+object_tangible_deed_ep3_pet_deed_varactyl_deed = object_tangible_deed_ep3_pet_deed_shared_varactyl_deed:new {
 
---Children folder includes
-includeFile("tangible/deed/base/serverobjects.lua")
-includeFile("tangible/deed/city_deed/serverobjects.lua")
-includeFile("tangible/deed/corellia/serverobjects.lua")
-includeFile("tangible/deed/event_perk/serverobjects.lua")
-includeFile("tangible/deed/faction_perk/serverobjects.lua")
-includeFile("tangible/deed/factory_deed/serverobjects.lua")
-includeFile("tangible/deed/generator_deed/serverobjects.lua")
-includeFile("tangible/deed/guild_deed/serverobjects.lua")
-includeFile("tangible/deed/harvester_deed/serverobjects.lua")
-includeFile("tangible/deed/naboo/serverobjects.lua")
-includeFile("tangible/deed/pet_deed/serverobjects.lua")
-includeFile("tangible/deed/player_house_deed/serverobjects.lua")
-includeFile("tangible/deed/turret_deed/serverobjects.lua")
-includeFile("tangible/deed/vehicle_deed/serverobjects.lua")
-includeFile("tangible/deed/ep3_pet_deed/serverobjects.lua")
--- Server Objects
-includeFile("tangible/deed/test_deed.lua")
+}
+
+ObjectTemplates:addTemplate(object_tangible_deed_ep3_pet_deed_varactyl_deed, "object/tangible/deed/ep3_pet_deed/varactyl_deed.iff")
