006b8cb8fa4e72c1f95046db7113003cc9944f88
mikeface <michael.cramerica@gmail.com>
2016-12-09 11:24:23 -0800

    Updated manufacturing schematic item limit

--- MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/sessions/crafting/CraftingSessionImplementation.cpp
@@ -1057,8 +1057,8 @@ void CraftingSessionImplementation::customization(const String& name, byte templ
 		}
 	}
 
-	if(schematicCount < 0 || schematicCount > 1000)
-		schematicCount = 1000;
+	if(schematicCount < 0 || schematicCount > 999)
+		schematicCount = 1000000;
 
 	manufactureSchematic->setManufactureLimit(schematicCount);
 
