c0f18940e039419d4df97200e9ea0dc1aa711ab5
Thrax989 <thrax989@yahoo.com>
2016-12-11 11:48:34 -0800

    [Added] Updated Object Installations

--- MMOCoreORB/bin/scripts/object/installation/turret/turret_dish_sm.lua
+++ MMOCoreORB/bin/scripts/object/installation/turret/turret_dish_sm.lua
@@ -50,7 +50,7 @@ object_installation_turret_turret_dish_sm = object_installation_turret_shared_tu
 	-- Damagetypes in WeaponObject
 	vulnerability = BLAST + LIGHTSABER,
 	-- LIGHT, MEDIUM, HEAVY
-	rating = LIGHT,
+	rating = HEAVY,
 
 	kinetic = 75,
 	energy = 95,
