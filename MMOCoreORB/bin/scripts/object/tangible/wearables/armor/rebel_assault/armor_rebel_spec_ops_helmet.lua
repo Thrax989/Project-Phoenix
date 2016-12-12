21e31be685620b8f27bd23b8064b5695f14bf3dc
Thrax989 <thrax989@yahoo.com>
2016-12-11 19:23:35 -0800

    [Added] 3 New Armor Suits Rebel Assault Rebel Assault Camo Rebel Assault GCW

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/rebel_assault/armor_rebel_spec_ops_helmet.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/rebel_assault/armor_rebel_spec_ops_helmet.lua
@@ -1,4 +1,4 @@
-object_tangible_wearables_armor_rebel_assault_armor_rebel_assault_helmet = object_tangible_wearables_armor_rebel_assault_shared_armor_rebel_assault_helmet:new {
+object_tangible_wearables_armor_rebel_assault_armor_rebel_spec_ops_helmet = object_tangible_wearables_armor_rebel_assault_shared_armor_rebel_spec_ops_helmet:new {
 
 	templateType = ARMOROBJECT,
 
@@ -91,4 +91,4 @@ object_tangible_wearables_armor_rebel_assault_armor_rebel_assault_helmet = objec
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
-ObjectTemplates:addTemplate(object_tangible_wearables_armor_rebel_assault_armor_rebel_assault_helmet, "object/tangible/wearables/armor/rebel/assault/armor_rebel_assault_helmet.iff")
+ObjectTemplates:addTemplate(object_tangible_wearables_armor_rebel_assault_armor_rebel_spec_ops_helmet, "object/tangible/wearables/armor/rebel/assault/armor_rebel_spec_ops_helmet.iff")
