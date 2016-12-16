bc2632eaecd6ec22af87b997ba0b6f4bcb6f4707
Thrax989 <thrax989@yahoo.com>
2016-12-15 23:09:53 -0800

    Revert "[Added] Missing Base Templates for cybernetics"
    This reverts commit 3d36c3d1b7e3af762bc535638650e88178ca350d.

--- MMOCoreORB/bin/scripts/object/tangible/wearables/base/base_cybernetic_legs.lua
+++
@@ -1,5 +0,0 @@
-object_tangible_wearables_base_base_cybernetic_legs = object_tangible_wearables_base_shared_base_cybernetic_legs:new {
-
-}
-
-ObjectTemplates:addTemplate(object_tangible_wearables_base_base_cybernetic_legs, "object/tangible/wearables/base/base_cybernetic_legs.iff")
--- MMOCoreORB/bin/scripts/object/tangible/wearables/base/cybernetic_base.lua
+++
@@ -1,5 +0,0 @@
-object_tangible_wearables_base_cybernetic_base = object_tangible_wearables_base_shared_cybernetic_base:new {
-
-}
-
-ObjectTemplates:addTemplate(object_tangible_wearables_base_cybernetic_base, "object/tangible/wearables/base/cybernetic_base.iff")
